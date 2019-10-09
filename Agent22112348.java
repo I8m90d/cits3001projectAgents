5ca30b046be3ec5f71780bd3aa9dfbd9ccc5be87
Gaverino <gavinvietnguyen2710@gmail.com>
2019-10-09 12:32:27 +0800

    Gav agents

---
+++ Agent22112348.java
@@ -0,0 +1,37 @@
+import loveletter.*;
+
+/**
+ * Write a description of class Agent22112348 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Agent22112348 implements Agent
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Agent22112348
+     */
+    public Agent22112348()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    public String toString(){
+        return "UWotMate?";
+    }
+    
+    public void newRound(State start){
+    
+    }
+    
+    public void see(Action act, State results){
+    }
+    
+    public Action playCard(Card c){
+        return null;
+    }
+}
